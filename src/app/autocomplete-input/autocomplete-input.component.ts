4230fb29655cfabd5616281b4205e330bcc02366
Ben <bjwhip@gmail.com>
2023-04-04 10:44:53 -0600

    refactor

--- src/app/autocomplete-input/autocomplete-input.component.ts
+++ src/app/autocomplete-input/autocomplete-input.component.ts
@@ -1,34 +1,74 @@
-import { Component, OnInit, ElementRef, ViewChild, Output, EventEmitter, NgZone, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
-declare var google: any;
+declare let google: any;
 
 @Component({
   selector: 'app-autocomplete-input',
   templateUrl: './autocomplete-input.component.html',
-  styleUrls: ['./autocomplete-input.component.scss']
+  styleUrls: ['./autocomplete-input.component.scss'],
 })
-export class AutocompleteInputComponent implements OnInit, AfterViewInit {
-  @ViewChild('autocompleteInput') autocompleteInput!: ElementRef;
+export class AutocompleteInputComponent implements OnInit {
   @Output() placeChanged: EventEmitter<any> = new EventEmitter();
+  
+  @ViewChild('searchInput') searchInput!: ElementRef;
+  private searchTextChanged = new Subject<string>();
 
-  autocomplete: any;
+  constructor() { }
 
-  constructor(private ngZone: NgZone) { }
+  ngOnInit(): void {
+    this.loadGoogleMapsApi().then((loaded) => {
+      if (loaded) {
+        this.initializeAutocomplete();
+      }
+    });
 
-  ngOnInit(): void { }
-
-  ngAfterViewInit(): void {
-    this.initAutocomplete();
+    this.searchTextChanged
+      .pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe((searchText) => this.searchAddress(searchText));
   }
 
-  initAutocomplete(): void {
-    this.autocomplete = new google.maps.places.Autocomplete(this.autocompleteInput.nativeElement, {});
+  private loadGoogleMapsApi(): Promise<boolean> {
+    return new Promise((resolve) => {
+      if (typeof google !== 'undefined' && google.maps && google.maps.places) {
+        resolve(true);
+      } else {
+        const script = document.createElement('script');
+        script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyA9PZH2zWK8cpwK0tQYPWzQCOjt8VxbqoI&libraries=places`;
+        script.async = true;
+        script.defer = true;
+        script.onload = () => resolve(true);
+        document.body.appendChild(script);
+      }
+    });
+  }
 
-    this.autocomplete.addListener('place_changed', () => {
-      this.ngZone.run(() => {
-        const place = this.autocomplete.getPlace();
-        this.placeChanged.emit(place);
-      });
+  private initializeAutocomplete(): void {
+    const autocomplete = new google.maps.places.Autocomplete(this.searchInput.nativeElement);
+    autocomplete.addListener('place_changed', () => {
+      const place = autocomplete.getPlace();
+      this.placeChanged.emit(place);
+      // console.log('Se:', place);
+    });
+  }
+
+  public onKeyUp(searchText: any): void {
+    this.searchTextChanged.next(searchText.target.value);
+  }
+
+  private searchAddress(searchText: string): void {
+    if (!searchText) {
+      return;
+    }
+
+    const autocompleteService = new google.maps.places.AutocompleteService();
+    autocompleteService.getPlacePredictions({ input: searchText }, (predictions: any, status: any) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        console.log('Address predictions:', predictions);
+      } else {
+        console.error('Error retrieving predictions:', status);
+      }
     });
   }
 }
